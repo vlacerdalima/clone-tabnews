1a2ea56b83e63338b1c65c2fb7a6fd2f6effc49f
Vitor Lacerda <vlacerdalima@gmail.com>
2025-12-18 14:26:20 +0000

    adiciona ca

--- infra/database.js
+++ infra/database.js
@@ -30,5 +30,10 @@ export default {
 
 function pegassl() {
   // futuramente precisando de um CA pode-se definir aq
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
   return process.env.NODE_ENV === "development" ? false : true;
 }
