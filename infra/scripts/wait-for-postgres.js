9da3e46ec1cc6e278ed7bd8adfa02683239750df
Vitor Lacerda <vlacerdalima@gmail.com>
2026-03-11 17:45:42 +0000

    automatiza a subida de servicos e evita RC

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,21 @@
+// objetivo daq é gerar uma flag para dizer se a db
+// está acessível para nao ter RC com as migrations
+
+const { exec } = require("node:child_process");
+
+function checa() {
+  exec("docker exec postgres-dev pg_isready --host localhost", lide);
+
+  function lide(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checa();
+      return;
+    }
+    // se chegou aqui é pq deu bom
+    console.log("\n Disponível para conexão!");
+  }
+}
+
+process.stdout.write("\n\nAguardando disponibilidade do psql.");
+checa();
