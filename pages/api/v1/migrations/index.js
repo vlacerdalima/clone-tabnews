77cad161b968dd7b98369a159cdb1c414ba94b0f
Vitor Lacerda <vlacerdalima@gmail.com>
2026-03-22 15:19:52 +0000

    Revert "fix: prevent production to not find dir route"
    This reverts commit 67998ad10cb18d9d8ca26fc005c8eed170d45f5e.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import { runner } from "node-pg-migrate";
-import { resolve } from "node:path";
+import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function aux(request, response) {
@@ -17,7 +17,7 @@ export default async function aux(request, response) {
       dbClient: client,
 
       dryRun: true,
-      dir: resolve("infra", "migrations"),
+      dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
