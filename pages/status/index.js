9588f72739a8e4da6a2b9bcba6669383a8fbd1d6
Vitor Lacerda <vlacerdalima@gmail.com>
2026-03-24 18:23:29 +0000

    feat: conclude `status` page MVP

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,26 +1,56 @@
 import useSWR from "swr";
-
+/* eslint-disable react/prop-types */
 async function fetchAPI(key) {
-  const response = fetch(key);
-  const corpo = (await response).json();
+  const response = await fetch(key);
+  const corpo = await response.json();
   return corpo;
 }
 
-export default function statusPage() {
+function Mostrar({ nome, valor, carregando }) {
   return (
-    <>
-      <h1>Status</h1>
-      <UpdatedAt />
-    </>
+    <div>
+      <strong>{nome}:</strong> {carregando ? "Carregando..." : valor}
+    </div>
   );
 }
 
-function UpdatedAt() {
-  const { isLoading, data } = useSWR("api/v1/status", fetchAPI, {
+function Horario({ valor, carregando }) {
+  let horarioFormatado = "Não carregado";
+  if (!carregando && valor) {
+    horarioFormatado = new Date(valor).toLocaleString("pt-BR");
+  }
+
+  return (
+    <Mostrar
+      nome="Última atualização"
+      valor={horarioFormatado}
+      carregando={carregando}
+    />
+  );
+}
+export default function statusPage() {
+  const { isLoading, data: resultado } = useSWR("api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
-  let agora = "Carregando...";
-  if (!isLoading && data)
-    agora = new Date(data.data_agr).toLocaleString("pt-BR");
-  return <div>Última verificação: {agora}</div>;
+  return (
+    <>
+      <h1>Status</h1>
+      <Horario valor={resultado?.data_agr} carregando={isLoading} />
+      <Mostrar
+        nome="Versão do Banco"
+        valor={resultado?.psql_version}
+        carregando={isLoading}
+      />
+      <Mostrar
+        nome="Conexões Máximas"
+        valor={resultado?.conexoes_maximas}
+        carregando={isLoading}
+      />
+      <Mostrar
+        nome="Conexões Ativas"
+        valor={resultado?.conexoes_agora}
+        carregando={isLoading}
+      />
+    </>
+  );
 }
