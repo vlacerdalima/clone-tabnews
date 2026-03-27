2d66bfd923a7dde349740bc2053f118baa67c328
Vitor Lacerda <vlacerdalima@gmail.com>
2026-03-27 12:30:17 +0000

    fix: handle undefined client in database.js

--- infra/errors.js
+++ infra/errors.js
@@ -1,20 +1,18 @@
 export class InternalServerError extends Error {
   constructor({ motivo_do_erro }) {
-    super("Erro interno, contate o suporte.", { 
-      cause: motivo_do_erro 
+    super("Erro interno, contate o suporte.", {
+      cause: motivo_do_erro,
     });
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte.";
     this.statusCode = 500;
-
   }
-  toJSON(){
+  toJSON() {
     // cria esse método para formatar melhor para o console
-    return{
-      name : this.name,
+    return {
+      name: this.name,
       action: this.action,
-      statusCode = this.statusCode
-
-    }
+      status_code: this.statusCode,
+    };
   }
 }
