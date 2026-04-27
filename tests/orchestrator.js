39d976bc9aae85acbe7945f9eea35e081ab9fcc7
Vitor Lacerda <vlacerdalima@gmail.com>
2026-04-27 13:34:33 +0000

    feat: adiciona infraestrutura local de Emails

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,15 +4,28 @@ import database from "infra/database.js";
 import migrator from "models/migrator.js";
 import user from "models/user.js";
 import session from "models/session";
+import email from "infra/email";
+
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
 
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
 
   async function waitForWebServer() {
     return retry(fetche, { retries: 100, maxTimeout: 1000 });
 
     async function fetche() {
       const resposta = await fetch("http://localhost:3000/api/v1/status");
+      if (resposta.status !== 200) throw Error();
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailPage, { retries: 100, maxTimeout: 1000 });
+
+    async function fetchEmailPage() {
+      const resposta = await fetch(emailHttpUrl);
       if (resposta.status !== 200) throw Error();
     }
   }
@@ -41,10 +54,30 @@ async function createSession(userId) {
   return await session.create(userId);
 }
 
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, { method: "DELETE" });
+}
+
+async function getLastEmail() {
+  const emailListResponse = await fetch(`${emailHttpUrl}/messages`);
+  const emailListBody = await emailListResponse.json();
+  const lastEmailItem = emailListBody.pop();
+
+  const emailTextResponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmailItem.id}.plain`,
+  );
+  const emailTextBody = await emailTextResponse.text();
+
+  lastEmailItem.text = emailTextBody;
+  return lastEmailItem;
+}
+
 export default {
   waitForAllServices,
   limpa,
   runPendingMigrations,
   createUser,
   createSession,
+  deleteAllEmails,
+  getLastEmail,
 };
